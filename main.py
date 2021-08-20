1ccacffa55683460f8323b092d798abc66783e52
Facundo García Martoni <thunderxforce@gmail.com>
2021-08-20 15:22:19 -0300

    deprecate_path_operation

--- main.py
+++ main.py
@@ -108,7 +108,8 @@ def create_person(person: Person = Body(...)):
 @app.get(
     path="/person/detail",
     status_code=status.HTTP_200_OK,
-    tags=["Persons"]
+    tags=["Persons"],
+    deprecated=True
     )
 def show_person(
     name: Optional[str] = Query(
