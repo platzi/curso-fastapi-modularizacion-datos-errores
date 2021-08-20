a58c167b447921c7ae19bb4d89fb6b129312f420
Facundo García Martoni <thunderxforce@gmail.com>
2021-08-20 12:36:58 -0300

    cookies_and_headers

--- main.py
+++ main.py
@@ -6,11 +6,13 @@ from enum import Enum
 #Pydantic
 from pydantic import BaseModel
 from pydantic import Field
+from pydantic import EmailStr
 
 #FastAPI
 from fastapi import FastAPI
 from fastapi import status
-from fastapi import Body, Query, Path, Form
+from fastapi import Body, Query, Path, Form, Header, Cookie
+from starlette.status import HTTP_200_OK
 from starlette.types import Message
 
 app = FastAPI()
@@ -144,6 +146,8 @@ def update_person(
     #return results
     return person
 
+# Forms
+
 @app.post(
     path="/login",
     response_model=LoginOut,
@@ -151,3 +155,30 @@ def update_person(
 )
 def login(username: str = Form(...), password: str = Form(...)): 
     return LoginOut(username=username)
+
+# Cookies and Headers Parameters
+
+@app.post(
+    path="/contact",
+    status_code=status.HTTP_200_OK
+)
+def contact(
+    first_name: str = Form(
+        ...,
+        max_length=20,
+        min_length=1
+    ),
+    last_name: str = Form(
+        ...,
+        max_length=20,
+        min_length=1
+    ),
+    email: EmailStr = Form(...),
+    message: str = Form(
+        ...,
+        min_length=20
+    ),
+    user_agent: Optional[str] = Header(default=None),
+    ads: Optional[str] = Cookie(default=None)
+): 
+    return user_agent
